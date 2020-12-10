b8d19e0fefc3fd88a13e2b408a61d4263627b835
turtleseverywhere <s.samele@libero.it>
2020-12-10 21:22:09 +0100

    soluzione

--- Program.cs
+++ Program.cs
@@ -8,8 +8,37 @@ namespace ConsoleApplicationsTDPC12
 {
     class Program
     {
+        /*
+             Scrivere un programma che:
+                 - chieda all'utente due numeri
+                 - scriva sullo schermo i risultati delle
+                 seguenti operazioni tra i due numeri:
+                     - addizione
+                     - sottrazione
+                     - moltiplicazione
+                     - divisione
+         */
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserisci il primo numero");
+            string number1AsString = Console.ReadLine();
+            decimal number1AsDecimal = decimal.Parse(number1AsString);
+
+            Console.WriteLine("Inserisci il secondo numero");
+            string number2AsString = Console.ReadLine();
+            decimal number2AsDecimal = decimal.Parse(number2AsString);
+
+            decimal addizione = number1AsDecimal + number2AsDecimal;
+            decimal sottrazione = number1AsDecimal - number2AsDecimal;
+            decimal moltiplicazione = number1AsDecimal * number2AsDecimal;
+            decimal divisione = number1AsDecimal / number2AsDecimal;
+
+            Console.WriteLine(number1AsString + " + " + number2AsString + " = " + addizione);
+            Console.WriteLine(number1AsString + " - " + number2AsString + " = " + sottrazione);
+            Console.WriteLine(number1AsString + " * " + number2AsString + " = " + moltiplicazione);
+            Console.WriteLine(number1AsString + " / " + number2AsString + " = " + divisione);
+
+            Console.ReadKey();
         }
     }
 }
