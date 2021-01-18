0850b034e30a17c1ac65f25e55d1be1c033cd0ab
turtleseverywhere <s.samele@libero.it>
2021-01-18 21:47:16 +0100

    esercizi

--- Program.cs
+++ Program.cs
@@ -8,6 +8,49 @@ namespace ConsoleApplicationsTDPC12
 {
     class Program
     {
+        /*
+         Scrivere un programma che:
+            - chieda all'utente una stringa
+            - stampi su schermo la lunghezza della stringa
+            - chieda all'utente una seconda stringa
+            - stampi su schermo la somma della lunghezza della prima e della lunghezza
+            della seconda
+         */
+        /*
+         Scrivere un programma che:
+            - dati 2 numeri:
+                - tramite una funzione scriva la somma fra i tre numeri
+                - tramite una seconda funzione scriva su schermo 
+                prima il maggiore e poi il minore
+        */
+        /*
+         Scrivere un programma che:
+            - chieda all'utente il lato di un quadrato
+            - tramite una funzione stampi su schermo l'area del quadrato
+            - tramite una seconda funzione stampi su schermo il perimetro del quadrato
+        */
+        /*
+         Scrivere un programma che:
+            - tramite una funzione randomizzi due numeri
+            - sommi i due numeri e stampi su schermo il risultato
+        */
+        /*
+         Scrivere un programma che:
+            - chieda all'utente nome e cognome
+            - tramite una funzione scriva su schermo nome e cognome dell'utente
+            - chieda all'utente un numero
+            - generi un numero casuale
+            - nel caso il numero dell'utente sia maggiore del numero casuale:
+                - scriva su schermo "Hai vinto"
+            - nel caso il numero dell'utente sia minore del numero casuale:
+                - scriva su schermo "Hai perso"
+        */
+        /*
+         Scrivere un programma che:
+            - chieda all'utente una stringa
+            - tramite una funzione stampi su schermo la stringa scritta in maiuscolo
+            - tramite una seconda funzione stampi su schermo la stringa scritta in minuscolo
+        */
         static void Main(string[] args)
         {
         }
