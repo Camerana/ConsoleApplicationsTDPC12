23596e9a2d529c3b1ebfc929e5348e33806b147c
turtleseverywhere <s.samele@libero.it>
2021-01-19 19:18:44 +0100

    esercizio

--- Program.cs
+++ Program.cs
@@ -8,8 +8,24 @@ namespace ConsoleApplicationsTDPC12
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-        }
-    }
+		/*
+		 * Termine: 19.30
+		 Scrivere un programma che:
+			- Data una funzione per stampare una stringa passata come parametro in ingresso,
+			chiami la suddetta funzione un numero n di volte scelto dall'utente
+		 */
+		static void Main(string[] args)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Eseguimi(i);
+			}
+			Console.ReadKey();
+		}
+
+		static void Eseguimi(int i)
+		{
+			Console.WriteLine("Ciao " + i);
+		}
+	}
 }
