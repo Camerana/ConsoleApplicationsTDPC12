0172cc4676cfb8bd5e3feae54169da735fe63008
turtleseverywhere <s.samele@libero.it>
2021-02-01 19:03:13 +0100

    i

--- Program.cs
+++ Program.cs
@@ -8,8 +8,27 @@ namespace ConsoleApplicationsTDPC12
 {
     class Program
     {
+        /*
+         Foreach:
+            - cicla su tutti gli elementi di una collection
+            - grammaticalmente più semplice del ciclo for
+         */
         static void Main(string[] args)
         {
+            List<string> stringhe = new List<string>();
+            stringhe.Add("Stringa 1");
+            stringhe.Add("Stringa 2");
+            stringhe.Add("Stringa 3");
+            stringhe.Add("Stringa 4");
+            stringhe.Add("Stringa 5");
+
+            for (int i = 0; i < stringhe.Count; i++)
+                Console.WriteLine(stringhe[i]);
+
+            foreach (string stringa in stringhe)
+                Console.WriteLine(stringa);
+
+            Console.ReadKey();
         }
     }
 }
