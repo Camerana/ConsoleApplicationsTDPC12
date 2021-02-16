fc8999de160b1f60178b52762bfcb6ec40bf1b94
turtleseverywhere <s.samele@libero.it>
2021-02-16 20:43:46 +0100

    ex

--- Auto.cs
+++ Auto.cs
@@ -8,7 +8,22 @@ namespace ConsoleApplicationsTDPC12
 {
     public class Auto
     {
-        public string Modello { get; set; }
+        private string modello;
+        public string Modello
+        {
+            get
+            {
+                if (modello == "Fiat 500")
+                    return this.modello;
+                else
+                    return "Non disponibile";
+            }
+            set
+            {
+                if (value != "Fiat 500")
+                    this.modello = value;
+            }
+        }
         public string Targa { get; set; }
         public int NumeroPosti { get; set; }
 
