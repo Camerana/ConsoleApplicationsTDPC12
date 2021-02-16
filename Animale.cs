88855e4b3658f01c8393352b7ef1023a164df84b
turtleseverywhere <s.samele@libero.it>
2021-02-16 21:33:24 +0100

    esercizio

---
+++ Animale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC12
+{
+    class Animale
+    {
+        public string Nome { get; set; }
+        public string Razza { get; set; }
+        public void Muoviti()
+        {
+            Console.WriteLine(this.Nome + " si sta muovendo");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,26 +9,33 @@ namespace ConsoleApplicationsTDPC12
     public class Program
     {
         /*
-         * Termine: 21.10
-                Creare una classe Animale con:
-                    - una property Nome
-                    - una property Razza
-                    - una funzione Muoviti
-                Creare 2 oggetti di tipo Animale
-                Attribuire tutti i valori ai due oggetti
-                Stampare su schermo le properties dei due oggetti
-                Chiamare la funzione Muoviti di entrambi gli oggetti
-
+            Creare una classe Animale con:
+                - una property Nome
+                - una property Razza
+                - una funzione Muoviti
+            Creare 2 oggetti di tipo Animale
+            Attribuire tutti i valori ai due oggetti
+            Stampare su schermo le properties dei due oggetti
+            Chiamare la funzione Muoviti di entrambi gli oggetti
          */
-
-
         static void Main(string[] args)
         {
-            Auto a = new Auto();
-            a.Modello = "Fiat 500";
+            Animale a1 = new Animale();
+            a1.Nome = "Ciccio";
+            a1.Razza = "Pesce rosso";
 
-            Console.WriteLine(a.Modello);
+            Animale a2 = new Animale();
+            a2.Nome = "Micio";
+            a2.Razza = "Cane";
 
+            Console.WriteLine(a1.Nome);
+            Console.WriteLine(a1.Razza);
+
+            Console.WriteLine(a2.Nome);
+            Console.WriteLine(a2.Razza);
+
+            a1.Muoviti();
+            a2.Muoviti();
 
             Console.ReadKey();
         }
