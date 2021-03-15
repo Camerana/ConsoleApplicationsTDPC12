54854bf2aa542b7f59a00a4d976997498a877534
turtleseverywhere <s.samele@libero.it>
2021-03-15 19:16:27 +0100

    initial

--- Program.cs
+++ Program.cs
@@ -8,8 +8,25 @@ namespace ConsoleApplicationsTDPC12
 {
     class Program
     {
+        /*
+         creazione di un array di 9 numeri da 1 a 9 disposti in modo casuale
+         e senza ripetizioni
+         */
         static void Main(string[] args)
         {
+            int[] numeri = new int[9];
+            Random r = new Random();
+
+            for (int i = 0; i < numeri.Length; i++)
+            {
+                int numero = r.Next(1, 10);
+                while (numeri.Contains(numero))
+                {
+                    numero = r.Next(1, 10);
+                }
+                numeri[i] = numero;
+            }
+            Console.ReadKey();
         }
     }
 }
