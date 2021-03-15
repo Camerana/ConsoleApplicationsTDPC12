0a5b84e8bf7341e6180ab2e338429d0ae8d469a6
turtleseverywhere <s.samele@libero.it>
2021-03-15 21:09:10 +0100

    ex

--- Program.cs
+++ Program.cs
@@ -9,9 +9,81 @@ namespace ConsoleApplicationsTDPC12
     class Program
     {
         /*
+         * Aggiornamento:   20.15
+         Esercitazione:
+            - Realizzare un clone del Tris in console:
+            |___|_O_|_X_|        
+            |___|_X_|_O_|
+            |_X_|___|_O_|
+            - Spunti in ordine di difficoltà:
+                - possibilità di tenere conto del punteggio di più partite        
+                - possibilità di multigiocatore sullo stesso terminale        
+                - possibilità di sfidare il computer                
+                - possibilità di multigiocatore su terminali diversi in rete
+         */
+        /*
+         TODO:
+            - implementare la condizione di vittoria quando un giocatore fa tris
+            - risolvere il bug per cui il computer va in loop se non ci sono caselle libere
+         */
+        static void Main(string[] args)
+        {
+            string[,] coordinate = new string[3, 3];
+            Render(coordinate);
+            Random r = new Random();
+            while (true)
+            {
+                bool sceltaValida = false;
+                while (!sceltaValida)
+                {
+                    Console.WriteLine("Inserisci le coordinate");
+                    string scelta = Console.ReadLine();
+                    int x = int.Parse(scelta[0].ToString());
+                    int y = int.Parse(scelta[1].ToString());
+                    if (coordinate[y, x] == null)
+                    {
+                        coordinate[y, x] = "X";
+                        sceltaValida = true;
+                    }
+                    Render(coordinate);
+                }
+                sceltaValida = false;
+                while (!sceltaValida)
+                {
+                    int x = r.Next(0, 3);
+                    int y = r.Next(0, 3);
+                    if (coordinate[x, y] == null)
+                    {
+                        coordinate[x, y] = "O";
+                        sceltaValida = true;
+                    }
+                    Render(coordinate);
+                }
+            }
+        }
+        private static void Render(string[,] coordinate)
+        {
+            Console.Clear();
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    string format = "|_P_|";
+                    if (coordinate[x, y] == null)
+                        format = format.Replace("P", "_");
+                    else
+                        format = format.Replace("P", coordinate[x, y]);
+                    Console.Write(format);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /*
          creazione di un array di 9 numeri da 1 a 9 disposti in modo casuale
          e senza ripetizioni
          */
+        /*
         static void Main(string[] args)
         {
             int[] numeri = new int[9];
@@ -28,5 +100,6 @@ namespace ConsoleApplicationsTDPC12
             }
             Console.ReadKey();
         }
+        */
     }
 }
